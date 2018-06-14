d8d83367fc1425525398b74ba8476fd95d73af2f
bao.zengkai <bao.zengkai@eisoo.com>
2018-06-14 21:46:17 +0800

    鲍增凯:增加JSONObject和JSONArray结合构造

--- java-core/src/main/java/com/utils/JSONDemo.java
+++ java-core/src/main/java/com/utils/JSONDemo.java
@@ -11,13 +11,31 @@ import org.json.JSONArray;
  *
  *      测试方法:
  *      1.将String类型转换为Json类型
+ *      2.使用JSONObject创建一个JSON
+ *      3.创建一个包含JSONArray的JSONObject
  */
 public class JSONDemo {
 
     public static void main(String[] args) {
-    // 1.使用mjson的read方法，将str转换为Json类型
-    String str = "{\"name\":\"xiaobao\",\"age\":24}";
-    Json json = Json.read(str);
+        // 1.使用mjson的read方法，将str转换为Json类型
+        String str = "{\"name\":\"xiaobao\",\"age\":24}";
+        Json json = Json.read(str);
 
+        //2.使用JSONObject创建一个JSON
+        Json json2  = Json.object().set("name","xiaobao").set("age",20);
+        String jsonStr = json2.toString();
+        System.out.println(jsonStr);
+
+        //3.创建一个包含JSONArray的JSONObject
+        Json child1 = Json.object().set("name","xiaobao");
+        Json child2 = Json.object().set("name","xiaoya");
+
+        Json father = Json.array().add(child1).add(child2);
+
+        Json grand_father = Json.object()
+                        .set("grand_child",father)
+                        .set("wife","grand_mather");
+
+        System.out.println(grand_father.toString());
     }
 }
