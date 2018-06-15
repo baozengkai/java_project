160700ef7de541f6e182616466dceb41a0aff557
bao.zengkai <bao.zengkai@eisoo.com>
2018-06-15 21:31:44 +0800

    鲍增凯:增加获取JSONObject属性代码

--- java-core/src/main/java/com/utils/JSONDemo.java
+++ java-core/src/main/java/com/utils/JSONDemo.java
@@ -13,6 +13,10 @@ import org.json.JSONArray;
  *      1.将String类型转换为Json类型
  *      2.使用JSONObject创建一个JSON
  *      3.创建一个包含JSONArray的JSONObject
+ *      4.获取其中的属性
+ *          4.1 获取简单的JSONObject对象数据，用at()方法转换为Json，然后在利用asXXX转换为相对应的String或者Integer
+ *          4.2 获取复杂的JSONArray对象数据，用asJsonlist()方法将其转换为List<Json>的方式，然后在for循环中处理即可
+ *                  或对于不复杂的JsonArray，利用asList()方法转换为List<Object>方式
  */
 public class JSONDemo {
 
@@ -31,11 +35,31 @@ public class JSONDemo {
         Json child2 = Json.object().set("name","xiaoya");
 
         Json father = Json.array().add(child1).add(child2);
+        Json mather = Json.array().add("eat").add("sleep");
 
         Json grand_father = Json.object()
-                        .set("grand_child",father)
-                        .set("wife","grand_mather");
+                .set("grand_child",father)
+                .set("wife","grand_mather")
+                .set("child",mather);
 
         System.out.println(grand_father.toString());
+
+        // 4.获取简单的JSONObject的属性
+        String name = json.at("name").asString();
+        System.out.println(name);
+
+        // 5.获取复杂的JSONArray的属性
+        for(Json person : grand_father.at("grand_child").asJsonList())
+        {
+            String name1 = person.at("name").asString();
+            System.out.println(name1);
+        }
+
+        //5.1如果是一些不复杂的可以利用asList()方法
+        for(Object obj : grand_father.at("child").asList())
+        {
+            String str1 = (String)obj;
+            System.out.println(str1);
+        }
     }
 }
